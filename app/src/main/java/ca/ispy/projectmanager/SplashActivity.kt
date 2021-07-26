62d0b517fb7b88dad9c1317ed0f7a06343537798
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-25 22:28:45 -0400

    Added custom font for splash screen -> fixed actionbar showing.

--- app/src/main/java/ca/ispy/projectmanager/SplashActivity.kt
+++ app/src/main/java/ca/ispy/projectmanager/SplashActivity.kt
@@ -1,11 +1,23 @@
 package ca.ispy.projectmanager
 
+import android.graphics.Typeface
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.insetsController?.hide(WindowInsets.Type.statusBars())
+
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "fonts/West Yard Free Trial.ttf")
+        tv_app_name.typeface = typeFace
     }
 }

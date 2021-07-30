e799011eb8aad0d87889ad24c36d6013bf6b4f2d
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-30 13:51:08 -0400

    Signup button bug causing crash fixed.

--- app/src/main/java/ca/ispy/projectmanager/IntroActivity.kt
+++ app/src/main/java/ca/ispy/projectmanager/IntroActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_intro.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class IntroActivity : AppCompatActivity() {
@@ -16,6 +17,6 @@ class IntroActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        btn_sign_up.setOnClickListener { startActivity(Intent(this, SignUpActivity::class.java)) }
+        btn_sign_up_intro.setOnClickListener { startActivity(Intent(this, SignUpActivity::class.java)) }
     }
 }

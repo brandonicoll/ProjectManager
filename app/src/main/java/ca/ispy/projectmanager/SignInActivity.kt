9a7d42e2d1d5a648c0f121143633ec71621ce46e
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-30 14:30:42 -0400

    Created SignInActivity.kt (not implemented) -> updated AndroidManifest.xml.

---
+++ app/src/main/java/ca/ispy/projectmanager/SignInActivity.kt
@@ -0,0 +1,11 @@
+package ca.ispy.projectmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}

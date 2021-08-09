4b0da823d7f3c801fecf3865f17be27290d74b73
Brandon <brandonspersonalmailbox@gmail.com>
2021-08-09 15:30:54 -0400

    Register user code implemented -> user saved to firebase with email and password id.

--- app/src/main/java/ca/ispy/projectmanager/activities/SignUpActivity.kt
+++ app/src/main/java/ca/ispy/projectmanager/activities/SignUpActivity.kt
@@ -6,6 +6,10 @@ import android.text.TextUtils
 import android.view.WindowManager
 import android.widget.Toast
 import ca.ispy.projectmanager.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : BaseActivity() { //extend base activity
@@ -46,11 +50,44 @@ class SignUpActivity : BaseActivity() { //extend base activity
 
         if (validateForm(name, email, password)) {
 
-            Toast.makeText(
-                this@SignUpActivity,
-                "User Registered",
-                Toast.LENGTH_SHORT
-            ).show()
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                    OnCompleteListener<AuthResult> { task ->
+
+                        // Hide the progress dialog
+                        hideProgressDialog()
+
+                        // If the registration is successfully done
+                        if (task.isSuccessful) {
+
+                            // Firebase registered user
+                            val firebaseUser: FirebaseUser = task.result!!.user!!
+                            // Registered Email
+                            val registeredEmail = firebaseUser.email!!
+
+                            Toast.makeText(
+                                this@SignUpActivity,
+                                "$name you have successfully registered with the email $registeredEmail.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            /**
+                             * Here the new user registered is automatically signed-in so we just sign-out the user from firebase
+                             * and send him to Intro Screen for Sign-In
+                             */
+                            FirebaseAuth.getInstance().signOut()
+                            // Finish the Sign-Up Screen
+                            finish()
+                        } else {
+                            Toast.makeText(
+                                this@SignUpActivity,
+                                task.exception!!.message,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    })
         }
     }
 
